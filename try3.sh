0640b553d90ed01a8bced9423cd6d63ad21039dc
Mike on Tecra <matroxmike@icloud.com>
2020-12-12 09:32:42 +1100

    making independent of user name

--- try3.sh
+++ try3.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-gcc -L/home/mike/cprogramming/ -Wl,-rpath=/home/mike/cprogramming/ -Wall -o test main.c -lfoo
+gcc -L~/foo -Wl,-rpath=~/foo -Wall -o test main.c -lfoo
 gcc -Wall -o test main.c -lfoo
-sudo cp /home/mike/cprogramming/libfoo.so /usr/lib
+sudo cp ~/cprogramming/libfoo.so /usr/lib
 whereis libfoo
 ldd test | grep foo
 ldconfig -p | grep foo
 ./test
 
+
+# replace /home/mike/ ~/  
--- tryThis1.sh
+++ tryThis1.sh
@@ -3,7 +3,7 @@ cd ~/cprogramming/    # change to working directory
 gcc -c -Wall -Werror -fpic foo.c    					# Step 1: Compiling with Position Independent Code
 gcc -shared -o libfoo.so foo.o						# Step 2: Creating a shared library from an object file
 gcc -Wall -o test main.c -lfoo						# Step 3: Linking with a shared library
-gcc -L/home/mike/foo -Wall -o/home/mike/cprogramming test main.c -lfoo 	# Telling GCC where to find the shared library
+gcc -L~/foo -Wall -o test main.c -lfoo 	# Telling GCC where to find the shared library
 echo $LD_LIBRARY_PATH
-objdump -p test
+# objdump -p test
 
