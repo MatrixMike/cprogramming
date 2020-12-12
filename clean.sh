87be853c42c07d8a8f3311ed75673cf4f9b774ba
Mike on Tecra <matroxmike@icloud.com>
2020-12-12 23:11:01 +1100

    shell clean up 1

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-rm *.so
+rm ./*glob*.so
 rm test
-rm *.o
+rm ./*glob*.o
 rm a.out
 #   globbing issue 
 # glob expands regex (etc) to list of things 
 # but I am using *.so -> better try using glob features -
 # will try later
+
+# 3.5.8.1 Pattern Matching  in https://www.gnu.org/software/bash/manual/bash.html#Shell-Expansions
+
 
