687baf0a6b31f84de98392ce20e26685c386110e
Mike Hewitt <matroxmike@icloud.com>
2020-12-12 12:56:33 +1100

    added pause to scripts

--- try3.sh
+++ try3.sh
@@ -2,9 +2,13 @@
 gcc -L~/foo -Wl,-rpath=~/foo -Wall -o test main.c -lfoo
 gcc -Wall -o test main.c -lfoo
 sudo cp ~/cprogramming/libfoo.so /usr/lib
+read -t 5 -p "waiting for 5 seconds 1"
 whereis libfoo
+read -t 5 -p "waiting for 5 seconds 2"
 ldd test | grep foo
+read -t 5 -p "waiting for 5 seconds 3"
 ldconfig -p | grep foo
+read -t 5 -p "waiting for 5 seconds 4"
 ./test
 
 
--- tryThis1.sh
+++ tryThis1.sh
@@ -2,8 +2,11 @@
 # cd ~/cprogramming/    # change to working directory
 cd ~/cprogramming/ || exit
 gcc -c -Wall -Werror -fpic foo.c    					# Step 1: Compiling with Position Independent Code
+read -t 5 -p "waiting for 5 seconds 1"
 gcc -shared -o libfoo.so foo.o						# Step 2: Creating a shared library from an object file
+read -t 5 -p "waiting for 5 seconds 2"
 gcc -Wall -o test main.c -lfoo						# Step 3: Linking with a shared library
+read -t 5 -p "waiting for 5 seconds 3"
 gcc -L~/foo -Wall -o test main.c -lfoo 	# Telling GCC where to find the shared library
 echo "$LD_LIBRARY_PATH"
 # objdump -p test
