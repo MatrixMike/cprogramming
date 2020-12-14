ae34ac3b133a5d834cd6607dafd16c19f845ac91
Mike Hewitt <matroxmike@icloud.com>
2020-12-14 23:04:30 +1100

    more work on shared libraries

--- try3.sh
+++ try3.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+sudo cp ~/cprogramming/libfoo.so /usr/lib
+sudo chmod 0755 /usr/lib/libfoo.so
 gcc -L~/foo -Wl,-rpath=~/foo -Wall -o test main.c -lfoo
 gcc -Wall -o test main.c -lfoo
 sudo cp ~/cprogramming/libfoo.so /usr/lib
-read -r -t 5 -p "\nwaiting for 5 seconds 1"
+read -r -t 1 -p "\nwaiting for 5 seconds 1"
 whereis libfoo
-read -r -t 5 -p "\nwaiting for 5 seconds 2"
-ldd test | grep foo
-read -r -t 5 -p "\nwaiting for 5 seconds 3"
+read -r -t 1 -p "\nwaiting for 5 seconds 2"
+
 ldconfig -p | grep foo
-read -r -t 5 -p "\nwaiting for 5 seconds 4"
+read -r -t 1 -p "\nwaiting for 5 seconds 4"
+unset LD_LIBRARY_PATH
+gcc -Wall -o test main.c -lfoo
+
+ldd test | grep foo
+read -r -t 1 -p "\nwaiting for 5 seconds 3"
 ./test
-
-
 # replace /home/mike/ ~/  
